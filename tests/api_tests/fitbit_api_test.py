9844d7591a4b0aa342f52fb21d7ffe98346f64e8
Pierre <pierre.marcel.zimermmann@gmail.com>
2024-11-06 11:49:33 +0100

    actually change how secrets are handled

--- tests/api_tests/fitbit_api_test.py
+++ tests/api_tests/fitbit_api_test.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from unittest.mock import patch, MagicMock
 from api.fitbit_api import FitbitAPI
 
@@ -7,10 +8,15 @@ class TestFitbitAPI(unittest.TestCase):
     def setUp(self):
         """
         Initializes the test setup with mock Fitbit credentials and test date.
+        Simulates environment variables for testing without actual secrets.
         """
-        self.fitbit_client_id = "test_client_id"  # Mock client ID
-        self.fitbit_client_secret = "test_client_secret"  # Mock client secret
-
+        # Simulate environment variables
+        os.environ['FITBIT_CLIENT_ID'] = 'test_client_id'
+        os.environ['FITBIT_CLIENT_SECRET'] = 'test_client_secret'
+        
+        self.fitbit_client_id = os.getenv('FITBIT_CLIENT_ID')
+        self.fitbit_client_secret = os.getenv('FITBIT_CLIENT_SECRET')
+        
         # Instantiate the FitbitAPI client (the authentication will be mocked)
         self.fitbit_api = FitbitAPI(self.fitbit_client_id, self.fitbit_client_secret)
         self.date = '2024-10-30'  # Test date for the API calls
@@ -22,15 +28,13 @@ class TestFitbitAPI(unittest.TestCase):
         Tests the get_heart_data method of the FitbitAPI client.
         Mocks the HTTP request to return fake heart rate data.
         """
-        mock_response = MagicMock()  # Create a mock response object
+        mock_response = MagicMock()
         expected_data = {'activities-heart-intraday': {'dataset': [{'time': '09:30:00', 'value': 70}]}}
-        mock_response.json.return_value = expected_data  # Return the expected data when json() is called
-        mock_get.return_value = mock_response  # Return the mock response when requests.get is called
+        mock_response.json.return_value = expected_data
+        mock_get.return_value = mock_response
 
-        # Call the method and assert the returned data matches the expected result
         heart_data = self.fitbit_api.get_heart_data(self.date)
         self.assertEqual(heart_data, expected_data)
-        # Assert that requests.get was called with the correct URL and authorization header
         mock_get.assert_called_once_with(
             f'https://api.fitbit.com/1/user/-/activities/heart/date/{self.date}/1d/1min.json',
             headers={'Authorization': 'Bearer test_access_token'}
