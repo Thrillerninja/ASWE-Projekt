50ffaf5f9379fe733bf41fea16cd82b3693f3bff
larswieck <138201015+larswieck@users.noreply.github.com>
2024-12-03 15:41:37 +0100

    small changes to petrol checker usecase regarding preferences

--- usecases/petrol_checker.py
+++ usecases/petrol_checker.py
@@ -2,16 +2,14 @@
 import api.notification_api.win_push_notification as notifier
 import api.petrol_api.main as petrol
 
-import main
-
-
 class PetrolChecker:
 
-    def __init__(self):
+    def __init__(self, state_machine):
+        self.state_machine = state_machine
         print("PetrolChecker initialized")
-        city = main.sm.preferences['home_location']['name']
-        fuel_name = main.sm.preferences['fuel_type']
-        fuel_radius = int(main.sm.preferences['fuel_radius'])
+        city = self.state_machine.preferences['home_location']['name']
+        fuel_name = self.state_machine.preferences['fuel_type']
+        fuel_radius = int(self.state_machine.preferences['fuel_radius'])
         self.petrol_api = petrol.PetrolAPI(city=city,
                                            fuel_name=fuel_name,
                                            range_km=fuel_radius)
@@ -25,12 +23,12 @@ class PetrolChecker:
         Checks if the current petrol price is below the threshold in the preferences
         """
         while True:
-            threshold_pref = main.sm.preferences['fuel_threshold']
+            threshold_pref = self.state_machine.preferences['fuel_threshold']
             if not self.notified:
                 self.last_notified_threshold = threshold_pref
 
             # Overwrite current price for demo purposes
-            overwrite_current_price = float(main.sm.preferences['fuel_demo_price'])
+            overwrite_current_price = float(self.state_machine.preferences['fuel_demo_price'])
             if overwrite_current_price > 0:
                 current_price_eur = overwrite_current_price
             else:
@@ -43,10 +41,8 @@ class PetrolChecker:
             # Notify if price is below threshold
             if current_price_eur < self.last_notified_threshold:
                 self.notified = True
-                self.last_notified_threshold -= main.sm.preferences['fuel_step_size']  # Notify every x cents decrease 
+                self.last_notified_threshold -= self.state_machine.preferences['fuel_step_size']  # Notify every x cents decrease 
                 self.notifier.push(title="Spritpreis gesunken",
                                    message=f"Der aktuelle Preis beträgt {current_price_eur}€",
                                    duration=10,
                                    threaded=True)
-
-
